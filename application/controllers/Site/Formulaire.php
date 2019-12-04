c24d331eb173a61c22d217b1c1dcc55fd9650818
RGXAlexandre <55505586+RGXAlexandre@users.noreply.github.com>
2019-12-04 16:16:49 +0100

    Création des controleurs indiv. Accueil, Categorie, Connexion, Formulaire, Panier, Produit.

--- application/controllers/Site/Formulaire.php
+++ application/controllers/Site/Formulaire.php
@@ -1,10 +1,11 @@
 <?php
 class Site extends CI_controller {
 
-    public function accueil () {
+    public function formulaire () {
         
         $this->load->view("header.php");
-        $this->load->view("accueil.php");
+        $this->load->view("formulaire.php");
         $this->load->view("footer.php");
     }
 }
+?>    
