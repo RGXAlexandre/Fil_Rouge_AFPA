1b50030407f08e9511d787e46aab66bf609c065e
RGXAlexandre <55505586+RGXAlexandre@users.noreply.github.com>
2019-12-10 09:56:47 +0100

    MAJ arboresence Controllers

--- application/controllers/Accueil.php
+++ application/controllers/Accueil.php
@@ -1,13 +1,8 @@
 <?php
 class Accueil extends CI_Controller {
     
-    public function index()
-    {
-        echo "Hello";
-    }
 
-    public function hello () {
-        echo "toto";
+    public function Hello () {
         $this->load->view("header.php");
         $this->load->view("accueil.php");
         $this->load->view("footer.php");
