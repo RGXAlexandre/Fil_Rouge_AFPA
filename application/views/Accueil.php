403df5446ce4b17f4cef0cf10b6338028d9a5ff3
RGXAlexandre <55505586+RGXAlexandre@users.noreply.github.com>
2019-12-10 10:13:32 +0100

    Validation de la mise en place de Bootstrap

---
+++ application/views/Accueil.php
@@ -0,0 +1,4 @@
+<h1>Coucou, je suis en Bootstrap =)</h1>
+
+<button type="button" class="btn btn-primary">Génial</button>
+<button type="button" class="btn btn-danger">Nul!</button>
---
+++ application/views/Footer.php
@@ -0,0 +1,4 @@
+<!-- Lien vers le CDN de JavaScript, necessaire au Boostrap présent dans le Header.php -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
