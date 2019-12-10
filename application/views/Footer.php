8b388fe5aad4023daef066086580c8cccb54715b
RGXAlexandre <55505586+RGXAlexandre@users.noreply.github.com>
2019-12-10 10:31:36 +0100

    Mise en place du fichier footer_simple.png dans Footer.php

--- application/views/Footer.php
+++ application/views/Footer.php
@@ -1,3 +1,9 @@
+<!-- Lien vers l'image footer SIMPLE.png, présente sur toutes les pages du site-->
+
+<img src="annexe/Charte/FOOTER/footer_simple" width="270" height="295" alt="footer_simple"/>
+
+
+
 <!-- Lien vers le CDN de JavaScript, necessaire au Boostrap présent dans le Header.php -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
