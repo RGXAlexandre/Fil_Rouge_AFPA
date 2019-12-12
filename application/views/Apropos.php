d38c333e0fc80db06568e379c7dddcd19f8ab15e
RGXAlexandre <55505586+RGXAlexandre@users.noreply.github.com>
2019-12-12 10:15:44 +0100

    Creation du bouton Espace Client dans Header.php, remplissage page Apropos.php et Aide.php

--- application/views/Apropos.php
+++ application/views/Apropos.php
@@ -1,2 +1,6 @@
-<h1>Contenu de Apropos.php</h1>
+<h1>A propos de GreenVillage</h1>
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad hic doloremque, dicta voluptatibus libero consequatur possimus, veniam provident ex sequi sint et quas quasi qui nobis accusamus, dignissimos repellat porro!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda impedit libero omnis earum ut ducimus enim, autem, quos soluta aperiam, adipisci sed iusto fugit id. Expedita est incidunt corrupti numquam.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nisi impedit corrupti quibusdam eligendi! A labore ipsam laboriosam error illum id dolor pariatur aperiam at! Repellat suscipit modi deleniti. Sint.
 <hr>
--- application/views/Header.php
+++ application/views/Header.php
@@ -43,7 +43,68 @@
                                 <a class="nav-link" href="<?php echo site_url("Accueil/Acc"); ?>">Accueil <span class="sr-only">(current)</span></a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="<?php echo site_url("Connexion/Con"); ?>">Connexion <span class="sr-only">(current)</span></a>
+                                <!-- Positionnement de data-toggle="modal" et  data-target="#myModal"dans le lien pour creer le point de depart de la Modal -->
+                                <a class="nav-link" href="" data-toggle="modal" data-target="#myModal">Espace Client</a>
+
+                                <!-- Début de la Modal -->
+                                <div id="myModal" class="modal fade" role="dialog">
+                                    <div class="modal-dialog">
+
+                                        <!-- Contenu de la Modal-->
+                                        <div class="modal-content">
+
+                                            <div class="modal-header">
+                                                <h4 class="modal-title">Espace Client</h4>
+                                            </div>
+
+                                            <div class="modal-body">
+                                                <div class="popForm popLeft">
+                                                    <span>Etes-vous déjà client chez nous ?</span>
+                                                    <form action="https://dev.amorce.org/al/fil_rouge/index.php/Customers/connexion" method="post" accept-charset="utf-8">
+
+                                                        <div class="form-group">
+                                                            <label for="user"></label>
+                                                            <input type="email" class="form-control" id="email" name="email" placeholder="Adresse E-mail">
+                                                            <label for="mdp"></label>
+                                                            <input type="password" class="form-control" id="password" name="password" placeholder="Mot de passe">
+                                                            <div class="form-check">
+                                                                <input type="checkbox" class="form-check-input" name="rememberMe" id="rememberMe">
+                                                                <label class="form-check-label" for="rememberMe">Rester connecté</label>
+                                                            </div>
+                                                        </div>
+                                                        <button type="submit" class="btn btn-success">Se connecter maintenant</button>
+                                                        <br>
+                                                        <a href="">Vous avez oublié votre mot de passe ?</a>
+
+                                                    </form>
+                                                </div>
+                                                <hr>
+                                                <div class="popForm popRight">
+                                                    <span>Vous n'êtes pas client chez nous ?</span>
+                                                    <br>
+                                                    <br>
+                                                    <p>En tant que client Village Green
+                                                        vous pouvez suivre vos envois,
+                                                        lire des tests produits exclusifs,
+
+                                                        évaluer des produits, déposer
+                                                        des petites annonces, gérer
+                                                        vos chèques-cadeaux, devenir
+                                                        partenaire et bien plus encore.</p>
+                                                    <a type="button" class="btn btn-success" href="<?php echo site_url("Formulaire/For"); ?>">S'inscrire</a>
+                                                    <br>
+                                                    <a href="<?php echo site_url("Apropos/Apr"); ?>">Plus d'informations</a>
+                                                </div>
+                                            </div>
+
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-danger" data-dismiss="modal">Fermer</button>
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+                                </div>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="<?php echo site_url("Panier/Pan"); ?>">Panier</a>
@@ -84,13 +145,16 @@
 
                         <ul class="navbar-nav ">
                             <li class="nav-item">
-                                <a class="nav-link" href="<?php echo site_url("Cat/Vents"); ?>">Instru. à vent <span class="sr-only">(current)</span></a>
+                                <a class="nav-link" href="<?= site_url('Categories/Cat2/') . 1 ?>">Les vents <span class="sr-only">(current)</span></a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="<?php echo site_url("Cat/Cordes"); ?>">Intru. à cordes <span class="sr-only">(current)</span></a>
+                                <a class="nav-link" href="<?= site_url('Categories/Cat2/') . 2 ?>">Les Guitares <span class="sr-only">(current)</span></a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="<?php echo site_url("Cat/Claviers"); ?>">Clavier</a>
+                                <a class="nav-link" href="<?= site_url('Categories/Cat2/') . 3 ?>">Christian Clavier</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?= site_url('Categories/Cat2/') . 4 ?>">Cordes frottées</a>
                             </li>
 
                         </ul>
