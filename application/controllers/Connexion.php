3e87cdcdf35e3df293802a23289d996caba31a65
RGXAlexandre <55505586+RGXAlexandre@users.noreply.github.com>
2019-12-18 15:13:46 +0100

    Validation de la connexion Client ou Employé

--- application/controllers/Connexion.php
+++ application/controllers/Connexion.php
@@ -3,16 +3,23 @@ class Connexion extends CI_controller
 {
 
 // Déclaration d'une méthode de Login (gérant un identifiant et d'un mot de passe), vers la librairie Basket.php, redirigeant vers l'accueil.
+// Mise en place d'une boucle pour le test de la table client er de la table employé, sion redirection vers une page d'échec.
     public function Log()
     {
         if ($this->input->post()) {
 
-            $e = $this->input->post("cli_mail");
-            $p = $this->input->post("cli_password");
+            $ec = $this->input->post("cli_mail");
+            $pc = $this->input->post("cli_password");
+            $ee = $this->input->post("cli_mail");
+            $pe = $this->input->post("cli_password");
 
-            if ($this->auth->login($e, $p, "client")) {
+            if ($this->auth->login($ec, $pc, "client")) {
                 redirect(site_url("Accueil/Acc"));
-            } else {
+            } 
+            else if ($this->auth->login($ee, $pe, "employé")){
+                redirect(site_url("Accueil/Acc"));
+            }
+            else {
                 redirect(site_url("Accueil/Perdu"));
             }
         }
